8d79659e0bafba5682afb392e0018c8d2ba2f9fc
OJ STUDIO LIVE <100084800+ojstudiolive@users.noreply.github.com>
2023-04-03 10:21:24 +0000

    added uer type selection for signup

--- src/routes/signup/Signup.jsx
+++ src/routes/signup/Signup.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignupForm from '../../components/auth/SignupForm'
+import EmployeeSignup from '../../components/auth/EmployeeSignup'
 import './css/Signup.scss'
 
 import Logo from '../../assets/geia-logo.png'
@@ -7,6 +8,16 @@ import Blob1 from '../../assets/blob/blob1.png'
 import Blob2 from '../../assets/blob/blob2.png'
 
 const Signup = () => {
+
+  const [userType, setUserType] = useState('admin')
+
+  const handleChange = e => {
+    const target = e.target;
+    if (target.checked) {
+      setUserType(target.value);
+    }
+  };
+
   return (
     <div className="signup_container">
       <div className="blob">
@@ -19,7 +30,13 @@ const Signup = () => {
             <img src={Logo} alt="" />
           </div>
           <div className="form_container">
-            <SignupForm />
+            <div className="select_user_login">
+              <div className="i_am_user"> <input type="radio" value="admin" id="amin" checked={userType === 'admin'} onChange={handleChange} /> &nbsp; I am an administrator</div>
+              <div className="i_am_user"> <input type="radio" value="employee" id="employee" checked={userType === 'employee'} onChange={handleChange} /> &nbsp; I am an employee</div>
+            </div>
+            <div className="form">
+              {userType === 'admin' ? <SignupForm /> : <EmployeeSignup />}
+            </div>
           </div>
         </div>
       </div>
